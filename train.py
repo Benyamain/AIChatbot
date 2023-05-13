bdbe16163d9c8a66bf27e09b32dae6aa0a16450e
Benyamain <yacoobbenyamain@gmail.com>
2023-05-13 15:35:47 -0400

    Created our dataset

--- train.py
+++ train.py
@@ -38,3 +38,23 @@ for (pattern_sentence, tag) in xy:
     y_train.append(label)
 
 X_train, y_train = np.array(X_train), np.array(y_train)
+
+class ChatDataset(Dataset):
+
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
+
+    # dataset[idx]
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+    
+    def __len__(self):
+        return self.n_samples
+    
+# Hyperparameters
+batch_size = 8
+
+dataset = ChatDataset()
+train_loader = DataLoader(dataset = dataset, batch_size = batch_size, shuffle = True, num_workers = 2)
