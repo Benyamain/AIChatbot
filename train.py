478cbe35c4039aa51a1185b32ab45fb609689e4c
Benyamain <yacoobbenyamain@gmail.com>
2023-05-13 15:52:10 -0400

    Almost finishing up!

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from model import NeuralNet
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
@@ -25,7 +26,6 @@ ignore_words = ['?', '!', '.', ',']
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
-print(tags)
 
 X_train, y_train = [], []
 
@@ -54,7 +54,23 @@ class ChatDataset(Dataset):
         return self.n_samples
     
 # Hyperparameters
-batch_size = 8
+# The first bag of words since they all have the same size
+BATCH_SIZE, HIDDEN_SIZE, OUTPUT_SIZE, INPUT_SIZE, LEARNING_RATE, NUM_EPOCHS = 8, 8, len(tags), len(X_train[0]), 0.001, 1000
 
 dataset = ChatDataset()
-train_loader = DataLoader(dataset = dataset, batch_size = batch_size, shuffle = True, num_workers = 2)
+train_loader = DataLoader(dataset = dataset, batch_size = BATCH_SIZE, shuffle = True, num_workers = 2)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = NeuralNet(INPUT_SIZE, HIDDEN_SIZE, OUTPUT_SIZE).to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr = LEARNING_RATE)
+
+for epoch in range(NUM_EPOCHS):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(device)
+
+        # Forward
+        outputs = model(words)
+        
